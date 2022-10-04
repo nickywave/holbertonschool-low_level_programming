7336434193a277c500be2ac13914053ac31a099e
Nickywave <nikoldolgopolova@gmail.com>
2022-10-04 05:19:49 -0700

    positive_or_negative

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main-entry point of the program where the execution begins
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+
+
+int main(void)
+{
+  int n;
+  
+
+  srand(time(0));
+  
+  n = rand() - RAND_MAX / 2;
+  if 
+  
+  return (0);
+}
