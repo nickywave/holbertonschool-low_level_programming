8b585d9d1c63977884dc13192d17eb0b13ee0537
Nickywave <nikoldolgopolova@gmail.com>
2022-10-05 13:32:26 -0700

    print_alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main () {
+   char ch;
+
+   for(ch = 'A' ; ch <= 'K' ; ch++) {
+      putchar(ch);
+   }
+   for(ch = 'l'; ch <= 'z' ; ch++) {
+           putchar(ch);
+   }
+   return(0);
+}
