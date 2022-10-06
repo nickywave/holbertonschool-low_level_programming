b452d98f798315b6bc9d6a7205281ec6be0f4d45
Nickywave <nikoldolgopolova@gmail.com>
2022-10-06 07:13:25 -0700

    nimberz

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-entry point of the program where the execution begins
+ *
+ *Return: Always 0 (Success)
+ *
+ */
+
+int main()
+{
+  
+  
+    char ch = '0';
+  
+   
+    for (ch = '0'; ch <= '9'; ch++)
+        putchar(ch);
+  
+    return (0);
+}
