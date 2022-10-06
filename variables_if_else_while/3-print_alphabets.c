5caa048891b68402a853b80fb1c897958b066b53
Nickywave <nikoldolgopolova@gmail.com>
2022-10-06 06:25:14 -0700

    print alphabet

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 
-int main () {
-   char ch;
+in - entry point of the program where the execution begins
+ *
+ *  * Return: Always 0 (Success)
+ *   *
+ *    */:char ch;
 
    for(ch = 'a' ; ch <= 'z' ; ch++) {
       putchar(ch);
    }
-   for(ch = 'A'; ch <= 'Z' ; ch++) {
+
            putchar(ch);
-   }
+
    return(0);
 }
---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program where the execution begins
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main () 
+{
+   char ch;
+
+   if ((ch !='q')&&ch !='e') 
+   for(ch = 'a' ; ch <= 'z' ; ch++)
+ {
+      putchar(ch);
+   }
+
+   return(0);
+}
