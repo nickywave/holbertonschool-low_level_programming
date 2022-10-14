4e90638a7db6bddfd0adfc70b2c0b1e72c99d7df
Nickywave <nikoldolgopolova@gmail.com>
2022-10-14 01:32:13 -0700

    task 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a given string in reverse order
+ * @s: pionter parameter
+ *
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int count = 0;
+
+	while (*s != '\0')
+	{
+		s++;
+		count++;
+	}
+	while (count > 0)
+	{
+		s--;
+		_putchar(*s);
+		count--;
+	}
+	_putchar('\n');
+}
