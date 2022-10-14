c9840c62853a3a1190969a6262f4a9d953d72fff
Nickywave <nikoldolgopolova@gmail.com>
2022-10-14 00:32:48 -0700

    task 2 pointers

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -8,4 +8,5 @@ void _puts(char *str);
 int _putchar(char);
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
 #endif
